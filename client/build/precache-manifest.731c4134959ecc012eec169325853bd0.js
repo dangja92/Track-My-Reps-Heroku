1188c2860db5a3e0ac03764f8d32673a5333cf1e
James_Dang <dangja@oregonstate.edu>
2022-08-31 23:36:41 -0700

    tested npm run build on client

---
+++ client/build/precache-manifest.731c4134959ecc012eec169325853bd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86e39a82a56a5b4a89c4",
+    "url": "/static/css/main.8920354e.chunk.css"
+  },
+  {
+    "revision": "86e39a82a56a5b4a89c4",
+    "url": "/static/js/main.86e39a82.chunk.js"
+  },
+  {
+    "revision": "2c80ac1fe2aa0d16567f",
+    "url": "/static/js/1.2c80ac1f.chunk.js"
+  },
+  {
+    "revision": "ae2b4517938dd63a84cc",
+    "url": "/static/js/2.ae2b4517.chunk.js"
+  },
+  {
+    "revision": "b7a5294dd25a4e7d9fd9",
+    "url": "/static/js/runtime~main.b7a5294d.js"
+  },
+  {
+    "revision": "d9103872fd01b3e03435f8a0aa0b9680",
+    "url": "/index.html"
+  }
+];
